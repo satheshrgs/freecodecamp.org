9fa15e07988e449d96af5efc857662b1705ec32c
srangasamy <chk-sathesh@hotmail.com>
2018-01-02 11:31:45 +0530

    Update quotes.js

--- files/quotes.js
+++ files/quotes.js
@@ -1,22 +1,22 @@
 var quotes = {
-    1:[
+    1:{
         "quote": "Any sufficiently advanced technology is equivalent to magic",
-        "author": "Arthur C. Clarke",
-    ],
-    2:[
+        "author": "Arthur C. Clarke"
+    },
+    2:{
         "quote": " Just because something doesn’t do what you planned it to do doesn’t mean it’s useless",
-        "author": "Thomas Edison",
-    ],
-    3:[
+        "author": "Thomas Edison"
+    },
+    3:{
         "quote": " It has become appallingly obvious that our technology has exceeded our humanity",
-        "author": "Albert Einstein",
-    ],
-    4:[
+        "author": "Albert Einstein"
+    },
+    4:{
         "quote": "Technology is a word that describes something that doesn’t work yet",
-        "author": "Douglas Adams",
-    ]
-    5:[
+        "author": "Douglas Adams"
+    }
+    5:{
         "quote": "Technology is anything that wasn’t around when you were born",
-        "author": "Alan Kay",
-    ]
+        "author": "Alan Kay"
+    }
 }
